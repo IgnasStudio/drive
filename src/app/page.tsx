fe7f330185ec406ba0fa16e15cb324125ea8259f
IgnasStudio <hi@ignas.studio>
2025-04-08 14:02:00 +0200

    adding placeholder homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,5 @@ import { db } from "~/server/db";
 import { files as fileSchema, folders as folderSchema } from "~/server/db/schema";
 
 export default async function GoogleDriveClone() {
-    const files = await db.select().from(fileSchema);
-    const folders = await db.select().from(folderSchema);
-    return <DriveContents files={files} folders={folders} />;
+  <div>Homepage</div>
 }
