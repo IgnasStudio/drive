11f3ffc3928e7f2d2d18b39cd224165a0091f641
IgnasStudio <hi@ignas.studio>
2025-04-11 16:05:46 +0200

    adding file upload

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,44 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { auth } from "@clerk/nextjs/server";
+import { MUTATIONS } from "~/server/db/queries";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+
+  imageUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    // Set permissions and file types for this FileRoute
+    .middleware(async () => {
+      const user = await auth();
+
+      if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.ufsUrl);
+      await MUTATIONS.createFile({
+        file: {
+            name: file.name,
+            size: file.size,
+            url: file.ufsUrl,
+            parent: 0, 
+            },
+            userId: metadata.userId,
+        });
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -2,6 +2,7 @@ import "server-only";
 
 import { db } from "~/server/db";
 import {
+  DB_FileType,
   files_table as fileSchema,
   folders_table as folderSchema,
 } from "~/server/db/schema";
@@ -36,3 +37,16 @@ export const QUERIES = {
     return db.select().from(fileSchema).where(eq(fileSchema.parent, folderId));
   },
 };
+
+export const MUTATIONS = {
+  createFile: async function (input: {
+    file: {
+      name: string;
+      size: number;
+      url: string;
+    };
+    userId: string;
+  }) {
+   return await db.insert(fileSchema).values({...input.file, parent: 1});
+  }
+}
